bad789883bac2049bee8f1749776937e7fa74caf
Timur Chitbaev <Sevxm-7ktvu-6rt2g-quqmp-7s67t-w3sq5-a6j7y-tgtb4-obi6f-iw2vk-wae@dmail.ai>
2025-08-16 23:47:14 +0300

    NMF-Акт Суверенитета

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+Import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android {
+    compileSdk = 34
+    namespace = "com.nmf.sovereignty"
+    defaultConfig {
+        applicationId = "com.nmf.sovereignty"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
+        archivesName = "${applicationId}-v${versionName}"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        release {
+            multiDexEnabled = true
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isDebuggable = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+        debug {
+            multiDexEnabled = true
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isDebuggable = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+    bundle {
+        storeArchive {
+            enable = true
+        }
+    }
+}
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("me.zhanghai.android.fastscroll:library:1.3.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
